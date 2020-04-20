3f90f00e8b6b37602325c7c82df982be986b8efd
olanetsoft <heedris2olubisi@gmail.com>
2020-04-20 16:28:39 +0100

    COMPLETED WITH PROT

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000 || process.env.PORT, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 });
